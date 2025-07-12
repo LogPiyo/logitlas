ca07181cd93a8208c56ddae09218db1c06c53259
LogPiyo <shushu20011031@gmail.com>
2025-07-13 01:03:27 +0900

    feat: replace mock data with external JSON file for theorems

--- src/app/mock.tsx
+++ src/app/mock.tsx
@@ -1,33 +1,9 @@
+import testData from './testData.json';
+
 export type Theorem = {
     theoremId: number;
     theoremName: string;
     dependencies: number[];
 }
 
-export const mockData: Theorem[] = [
-    {
-        theoremId: 1,
-        theoremName: "1.1",
-        dependencies: [],
-    },
-    {
-        theoremId: 2,
-        theoremName: "1.2",
-        dependencies: [1],
-    },
-    {
-        theoremId: 3,
-        theoremName: "1.3",
-        dependencies: [1, 2],
-    },
-    {
-        theoremId: 4,
-        theoremName: "1.4",
-        dependencies: [2],
-    },
-    {
-        theoremId: 5,
-        theoremName: "1.5",
-        dependencies: [3, 4],
-    },
-];
+export const mockData: Theorem[] = testData;
